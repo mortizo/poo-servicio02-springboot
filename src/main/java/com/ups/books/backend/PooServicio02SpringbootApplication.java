9ae616f9c585b226eb203c37d0462d85e9467bfa
DESKTOP-8FGL23A\UPS <mortizo@hotmail.com>
2022-07-25 22:03:58 -0500

    Commit inicial

---
+++ src/main/java/com/ups/books/backend/PooServicio02SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ups.books.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooServicio02SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooServicio02SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ups/books/backend/PooServicio02SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ups.books.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooServicio02SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
