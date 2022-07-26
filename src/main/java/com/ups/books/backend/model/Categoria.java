bf85e1eccc7d87efe0676a53e3092e8c6bb441ec
DESKTOP-8FGL23A\UPS <mortizo@hotmail.com>
2022-07-25 22:13:30 -0500

    Modelo de Categoria y Libro con el repositorio

---
+++ src/main/java/com/ups/books/backend/model/Categoria.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ups.books.backend.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+
+	/*
+	 * Mauricio Ortiz
+	 * UPS
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nombre;
+	private String descripcion;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
---
+++ src/main/java/com/ups/books/backend/model/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ups.books.backend.model.repository;
+
+import com.ups.books.backend.model.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author UPS
+ */
+public interface CategoriaRepository extends CrudRepository<Categoria, Long>{
+    
+}
