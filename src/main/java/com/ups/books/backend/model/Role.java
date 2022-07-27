04c73d22af10699f88b62fe9d8418282cce44ed9
mortizo <mortizo@ups.edu.ec>
2022-07-27 12:33:26 -0500

    Model de Role y Usuario

---
+++ src/main/java/com/ups/books/backend/model/Role.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ups.books.backend.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author morti
+ */
+@Entity
+@Table(name = "usuario")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String nombreRole;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreRole() {
+        return nombreRole;
+    }
+
+    public void setNombreRole(String nombreRole) {
+        this.nombreRole = nombreRole;
+    }
+
+
+    
+}
