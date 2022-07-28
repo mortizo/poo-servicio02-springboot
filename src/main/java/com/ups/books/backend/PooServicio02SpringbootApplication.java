d5ef71933eb48de66826313593250637d5d9dec1
DESKTOP-8FGL23A\UPS <mortizo@hotmail.com>
2022-07-27 22:55:51 -0500

    documentación con SWAGGER

--- src/main/java/com/ups/books/backend/PooServicio02SpringbootApplication.java
+++ src/main/java/com/ups/books/backend/PooServicio02SpringbootApplication.java
@@ -1,30 +1,32 @@
 package com.ups.books.backend;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class PooServicio02SpringbootApplication implements CommandLineRunner{
-
+public class PooServicio02SpringbootApplication //implements CommandLineRunner 
+{
+/*
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-    
-	public static void main(String[] args) {
-		SpringApplication.run(PooServicio02SpringbootApplication.class, args);
-	}
-
+        
     @Override
     public void run(String... args) throws Exception {
         String password = "123456";
-        
-        for(int i=0;i<20;i++){
+
+        for (int i = 0; i < 20; i++) {
             String passwordBcrypt = this.passwordEncoder.encode(password);
             System.out.println(passwordBcrypt);
         }
     }
+*/
+    public static void main(String[] args) {
+        SpringApplication.run(PooServicio02SpringbootApplication.class, args);
+    }
+
+
 
 }
