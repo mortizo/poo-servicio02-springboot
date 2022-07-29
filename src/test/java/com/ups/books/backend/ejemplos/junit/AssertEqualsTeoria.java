14730755bf0dcf96a2c95ce6c1412cac29968302
mortizo <mortizo@ups.edu.ec>
2022-07-29 14:56:17 -0500

    Modifica pom.xml para trabajar con JUnit

---
+++ src/test/java/com/ups/books/backend/ejemplos/junit/AssertEqualsTeoria.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ups.books.backend.ejemplos.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author morti
+ */
+public class AssertEqualsTeoria {
+    
+    @Test
+    public void miTest(){
+        assertEquals(1,1,0);
+  //      assertEquals(1,2,0);
+        
+    }
+    
+}
