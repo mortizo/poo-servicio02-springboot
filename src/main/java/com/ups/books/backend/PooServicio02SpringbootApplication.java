8582aaa95ce7413eb81825029af91f95536a025a
DESKTOP-8FGL23A\UPS <mortizo@hotmail.com>
2023-02-16 21:52:04 -0500

    Hola Mundo

--- src/main/java/com/ups/books/backend/PooServicio02SpringbootApplication.java
+++ src/main/java/com/ups/books/backend/PooServicio02SpringbootApplication.java
@@ -29,14 +29,6 @@ public class PooServicio02SpringbootApplication //implements CommandLineRunner
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        //calcular el factorial de un numero
-        int numero = 10;
-
-        int factorial = 1;
-        for (int i = 1; i <= numero; i++) {
-            factorial = factorial * i;
-        }
-        System.out.println("El factorial de " + numero + " es: " + factorial);
 
     }
 
